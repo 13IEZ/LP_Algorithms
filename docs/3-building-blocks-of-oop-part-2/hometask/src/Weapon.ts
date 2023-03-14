6e06a130f9435c190d6eda43de8291f2cb0f1402
Igor Zakharov <igor_zakharov3@epam.com>
2023-03-14 19:43:29 +0600

    hometask_2: resolved comments

--- docs/3-building-blocks-of-oop-part-2/hometask/src/Weapon.ts
+++ docs/3-building-blocks-of-oop-part-2/hometask/src/Weapon.ts
@@ -20,6 +20,7 @@ export abstract class Weapon extends Item {
     const isWillBeBroke = this.baseDurability - Weapon.MODIFIER_CHANGE_RATE < Weapon.MODIFIER_CHANGE_RATE;
     const isBroken = this.baseDurability < Weapon.MODIFIER_CHANGE_RATE;
     const isActive = this.baseDurability > Weapon.MODIFIER_CHANGE_RATE;
+    const defaultString = `You use the ${this.name}, dealing ${Weapon.MODIFIER_CHANGE_RATE} points of damage.`;
 
     if(isBroken) {
       return `You can't use the ${this.name}, it is broken.`;
@@ -27,12 +28,12 @@ export abstract class Weapon extends Item {
 
     if(isWillBeBroke) {
       this.baseDurability -= Weapon.MODIFIER_CHANGE_RATE;
-      return `You use the ${this.name}, dealing ${Weapon.MODIFIER_CHANGE_RATE} points of damage.\nThe ${this.name} breaks.`
+      return `${defaultString}\nThe ${this.name} breaks.`
     }
 
     if(isActive) {
       this.baseDurability -= Weapon.MODIFIER_CHANGE_RATE;
-      return `You use the ${this.name}, dealing ${Weapon.MODIFIER_CHANGE_RATE} points of damage.`;
+      return defaultString;
     }
   }
 
